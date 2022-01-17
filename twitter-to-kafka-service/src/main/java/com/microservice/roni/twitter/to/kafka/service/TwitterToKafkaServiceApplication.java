efb3abb28fb78514f1fa44083cf7ea339164cef4
Roni <ronicse2011@gmail.com>
2022-01-17 18:03:39 +0600

    added config file

--- twitter-to-kafka-service/src/main/java/com/microservice/roni/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservice/roni/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,19 +1,20 @@
 package com.microservice.roni.twitter.to.kafka.service;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-public class TwitterToKafkaServiceApplication {
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
     }
 
-    @PostConstruct
-    public void init() {
-
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("App starts...");
     }
 }
